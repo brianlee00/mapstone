3afe5af015e189bc504b879ed784c65befe41156
Ryjll Morris <ryjryjm@gmail.com>
2022-08-22 14:33:29 -0400

    Models Interfaces

--- cyburbia/src/main/java/learn/cyburbia/data/AgencyRepository.java
+++ cyburbia/src/main/java/learn/cyburbia/data/AgencyRepository.java
@@ -1,4 +1,20 @@
 package learn.cyburbia.data;
 
+import learn.cyburbia.models.Agency;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 public interface AgencyRepository {
+    List<Agency> findAll();
+
+    Agency findById(int agencyId);
+
+    Agency add(Agency agency);
+
+    boolean update(Agency agency);
+
+    @Transactional
+    boolean deleteById(int agencyId);
+
 }
