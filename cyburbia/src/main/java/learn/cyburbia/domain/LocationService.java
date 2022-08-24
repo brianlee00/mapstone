f47cc35d7142ba1744e86ce3301b38a28905171f
Ryjll Morris <ryjryjm@gmail.com>
2022-08-24 12:35:20 -0400

    Location Service and Tests

--- cyburbia/src/main/java/learn/cyburbia/domain/LocationService.java
+++ cyburbia/src/main/java/learn/cyburbia/domain/LocationService.java
@@ -1,4 +1,102 @@
 package learn.cyburbia.domain;
 
-public class LocationService {
+import learn.cyburbia.data.LocationRepository;
+import learn.cyburbia.models.Location;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+    public class LocationService {
+
+        private final LocationRepository repository;
+
+        public LocationService(LocationRepository repository) {
+            this.repository = repository;
+        }
+
+        public List<Location> findAll() {return repository.findAll();}
+
+        public Location findById(int locationId) {
+            return repository.findById(locationId);
+        }
+
+        public Result<Location> add(Location location) {
+            Result<Location> result = validate(location);
+            if (!result.isSuccess()) {
+                return result;
+            }
+
+            if (location.getLocationId() != 0) {
+                result.addMessage("locationId cannot be set for `add` operation", ResultType.INVALID);
+                return result;
+            }
+
+            location = repository.add(location);
+            result.setPayload(location);
+            return result;
+        }
+
+        public Result<Location> update(Location location) {
+            Result<Location> result = validate(location);
+            if (!result.isSuccess()) {
+                return result;
+            }
+
+            if (location.getLocationId() <= 0) {
+                result.addMessage("locationId must be set for `update` operation", ResultType.INVALID);
+                return result;
+            }
+
+            if (!repository.update(location)) {
+                String msg = String.format("locationId: %s, not found", location.getLocationId());
+                result.addMessage(msg, ResultType.NOT_FOUND);
+            }
+
+            return result;
+        }
+
+
+        private Result<Location> validate(Location location) {
+            Result<Location> result = new Result<>();
+
+            if (location == null) {
+                result.addMessage("Location Cannot be Null", ResultType.INVALID);
+                return result;
+            }
+
+            if (Validations.isNullOrBlank(location.getAddress())) {
+                result.addMessage("Address is Required", ResultType.INVALID);
+            }
+
+            if (Validations.isNullOrBlank(location.getCity())) {
+                result.addMessage("City is Required", ResultType.INVALID);
+            }
+
+            if (location.getState() == null) {
+                result.addMessage("State is Required", ResultType.INVALID);
+            }
+
+            if (Validations.isNullOrBlank(location.getZipCode())) {
+                result.addMessage("Zip Code is required", ResultType.INVALID);
+            }
+
+            result = validateDuplicate(location, result);
+
+            return result;
+        }
+        private Result<Location> validateDuplicate(Location location, Result<Location> result) {
+            List<Location> all = repository.findAll();
+
+            for (Location l : all) {
+                if (l.getLocationId() == location.getLocationId() && l.getAddress().equalsIgnoreCase(location.getAddress()) && l.getCity().equalsIgnoreCase(location.getCity()) && l.getState() == location.getState() && l.getZipCode().equalsIgnoreCase(location.getZipCode())) {
+                    result.addMessage("Duplicate Location not Allowed", ResultType.INVALID);
+
+                } else if (l.getLocationId() != location.getLocationId() && l.getAddress().equalsIgnoreCase(location.getAddress()) && l.getCity().equalsIgnoreCase(location.getCity()) && l.getState() == location.getState() && l.getZipCode().equalsIgnoreCase(location.getZipCode())) {
+                    result.addMessage("Duplicate Location not Allowed", ResultType.INVALID);
+                }
+            }
+            return result;
+        }
+
 }
---
+++ cyburbia/src/test/java/learn/cyburbia/domain/LocationServiceTest.java
@@ -0,0 +1,118 @@
+package learn.cyburbia.domain;
+import learn.cyburbia.data.LocationRepository;
+import learn.cyburbia.models.Location;
+import learn.cyburbia.models.State;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class LocationServiceTest {
+
+    @Autowired
+    LocationService service;
+
+    @MockBean
+    LocationRepository repository;
+
+    @Test
+    void shouldNotAddWhenInvalid() {
+        Location location = makeLocation();
+        location.setAddress(null);
+        Result<Location> actual = service.add(location);
+        assertEquals(ResultType.INVALID, actual.getType());
+
+        location = makeLocation();
+        location.setCity("\t");
+        actual = service.add(location);
+        assertEquals(ResultType.INVALID, actual.getType());
+
+        location = makeLocation();
+        location.setState(null);
+        actual = service.add(location);
+        assertEquals(ResultType.INVALID, actual.getType());
+
+        location = makeLocation();
+        location.setZipCode(" ");
+        actual = service.add(location);
+        assertEquals(ResultType.INVALID, actual.getType());
+    }
+    @Test
+    void shouldNotAddDuplicate() {
+        Location location = makeLocation();
+        Location mockOut = makeLocation();
+        mockOut.setLocationId(1);
+
+        when(repository.add(location)).thenReturn(mockOut);
+
+        Location locationTwo = makeLocation();
+        locationTwo.setLocationId(2);
+
+        Result<Location> actual = service.add(location);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+
+        Result<Location> actualTwo = service.add(locationTwo);
+        assertEquals(ResultType.INVALID, actualTwo.getType());
+
+
+
+    }
+
+    @Test
+    void shouldAdd() {
+        Location location = makeLocation();
+        Location mockOut = makeLocation();
+        mockOut.setLocationId(1);
+
+        when(repository.add(location)).thenReturn(mockOut);
+
+        Result<Location> actual = service.add(location);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+        assertEquals(mockOut, actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateWhenInvalid() {
+        Location location = makeLocation();
+        Result<Location> actual = service.update(location);
+        assertEquals(ResultType.INVALID, actual.getType());
+
+        location = makeLocation();
+        location.setLocationId(1);
+        location.setZipCode(" ");
+        actual = service.update(location);
+        assertEquals(ResultType.INVALID, actual.getType());
+
+        location = makeLocation();
+        location.setLocationId(1);
+        location.setCity(" ");
+        actual = service.update(location);
+        assertEquals(ResultType.INVALID, actual.getType());
+    }
+
+
+    @Test
+    void shouldUpdate() {
+        Location location = makeLocation();
+        location.setLocationId(1);
+
+        when(repository.update(location)).thenReturn(true);
+
+        Result<Location> actual = service.update(location);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+    }
+
+    Location makeLocation() {
+        Location location = new Location();
+        location.setAddress("Testing Street");
+        location.setCity("Test City");
+        location.setState(State.NEW_YORK);
+        location.setZipCode("55555-PST");
+        return location;
+    }
+
+}
