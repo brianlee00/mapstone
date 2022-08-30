4f672c50654546d54434d1245e4cb90c27eaa6e0
Stephen <stephen.yale@icloud.com>
2022-08-30 11:31:09 -0400

    no compilation errors, rename Project List to Projects

--- cyburbia-react/src/components/Home.js
+++ cyburbia-react/src/components/Home.js
@@ -7,9 +7,9 @@ function Home() {
   return (
     <>
     <div className="container">
-      <h2 className="mt-3">Home</h2>
+      {/* <h2 className="mt-3">Home</h2> */}
 
-      <h4 className="mb-3 mt-3 ml-3">Welcome to the Cyburbia Urban Development Map</h4>
+      <h4 className="mb-3 mt-3">Welcome to the Cyburbia Urban Development Map</h4>
       </div>
       <div className="container">
         <Map />
