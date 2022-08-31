1438fb512168d873ebd711f91bcaad53bdf2c05c
Stephen <stephen.yale@icloud.com>
2022-08-31 18:33:20 -0400

    JWT token implemented

---
+++ cyburbia/src/main/java/learn/cyburbia/AppConfig.java
@@ -0,0 +1,32 @@
+package learn.cyburbia;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        // Configure CORS globally versus
+        // controller-by-controller.
+        // Can be combined with @CrossOrigin.
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
