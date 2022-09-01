cacf668b7a38651ad2fd31fb9930a8a0d36fc4be
Stephen <stephen.yale@icloud.com>
2022-09-01 15:24:16 -0400

    registration works and auto-logs in

--- cyburbia-react/src/components/Register.js
+++ cyburbia-react/src/components/Register.js
@@ -32,8 +32,30 @@ export default function Register() {
         });
 
         if (response.status === 201) {
-            history.push("/login");
-        } else if (response.status === 403) {
+            const response = await fetch("http://localhost:8080/authenticate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                }),
+            });
+
+            // This code executes if the request is successful
+            if (response.status === 200) {
+                const { jwt_token } = await response.json();
+                console.log(jwt_token);
+                // NEW: login!
+                auth.login(jwt_token);
+                history.push("/");
+            } else if (response.status === 403) {
+                setErrors(["Login failed."]);
+            } else {
+                setErrors(["Unknown error."]);
+            }
+        } else if (response.status === 400) {
             setErrors(["Login failed."]);
         } else {
             setErrors(["Unknown error."]);
@@ -44,34 +66,34 @@ export default function Register() {
 
 
 
-return (
-    <div className="container">
-        <h2 className="mt-3">Register</h2>
-        {errors.map((error, i) => (
-            <Error key={i} msg={error} />
-        ))}
-        <form className="form-group" onSubmit={handleSubmit}>
-            <div>
-                {/* Includes for/id attributes for basic HTML accessibility ♿. */}
-                <label htmlFor="username">Username: </label>
-                <input className="form-control"
-                    type="text"
-                    onChange={(event) => setUsername(event.target.value)}
-                    id="username"
-                />
-            </div>
-            <div className="mt-3">
-                <label htmlFor="password">Password: </label>
-                <input className="form-control"
-                    type="password"
-                    onChange={(event) => setPassword(event.target.value)}
-                    id="password"
-                />
-            </div>
-            <div>
-                <button className="btn btn-success mt-3" type="submit">Create New User</button>
-            </div>
-        </form>
-    </div>
-);
+    return (
+        <div className="container">
+            <h2 className="mt-3">Register</h2>
+            {errors.map((error, i) => (
+                <Error key={i} msg={error} />
+            ))}
+            <form className="form-group" onSubmit={handleSubmit}>
+                <div>
+                    {/* Includes for/id attributes for basic HTML accessibility ♿. */}
+                    <label htmlFor="username">Username: </label>
+                    <input className="form-control"
+                        type="text"
+                        onChange={(event) => setUsername(event.target.value)}
+                        id="username"
+                    />
+                </div>
+                <div className="mt-3">
+                    <label htmlFor="password">Password (must be at least 8 characters and contain a digit, a letter, and a special character): </label>
+                    <input className="form-control"
+                        type="password"
+                        onChange={(event) => setPassword(event.target.value)}
+                        id="password"
+                    />
+                </div>
+                <div>
+                    <button className="btn btn-success mt-3" type="submit">Create New User</button>
+                </div>
+            </form>
+        </div>
+    );
 }
