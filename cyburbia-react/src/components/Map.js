212bfb5c750c07ce12649c9cf444da79bcb02647
Brian Lee <bjlee0304@gmail.com>
2022-08-31 21:06:48 -0400

    popup on click

--- cyburbia-react/src/components/Map.js
+++ cyburbia-react/src/components/Map.js
@@ -17,7 +17,6 @@ function Map() {
     if (map.current) {return};
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-74.0060, 40.7128],
       zoom: 10
@@ -36,45 +35,44 @@ function Map() {
       })
       .then(data => 
         data.map(project =>
-        fetch(`http://localhost:8080/api/location/${project.locationId}`)
-        .then(response => {
-          if (response.status === 200) {
-            return response.json();
-          } else {
-            return Promise.reject(`Unexpected status code: ${response.status}`);
-          }
-      })
-      .then(data =>
-        mapboxClient.forwardGeocode({
-          query: data.address.concat(", ",data.city,", ",data.state," ",data.zipCode),
-          autocomplete: false,
-          limit: 10
-        })
-        .send()
-        .then((response) => {
-          if (
-            !response ||
-            !response.body ||
-            !response.body.features ||
-            !response.body.features.length
-          ) {
-            console.error('Invalid response:');
-            console.error(response);
-            return;
-          }
-          const feature = response.body.features[0];
-          // Create a marker and add it to the map.
-          new mapboxgl.Marker().setLngLat(feature.center).addTo(map.current);
-          const popup = new mapboxgl.Popup({ closeOnClick: false, closeOnMove: false })
-            .setLngLat(feature.center)
-            .setHTML('<h1>This Location Details</h1>')
-            .addTo(map.current);
-        })
-      ))
+          fetch(`http://localhost:8080/api/location/${project.locationId}`)
+          .then(response => {
+            if (response.status === 200) {
+              return response.json();
+            } else {
+              return Promise.reject(`Unexpected status code: ${response.status}`);
+            }
+          })
+          .then(data =>
+            mapboxClient.forwardGeocode({
+              query: data.address.concat(", ",data.city,", ",data.state," ",data.zipCode),
+              autocomplete: false,
+              limit: 10
+            })
+              .send()
+              .then((response) => {
+                if (
+                  !response ||
+                  !response.body ||
+                  !response.body.features ||
+                  !response.body.features.length
+                ) {
+                  console.error('Invalid response:');
+                  console.error(response);
+                  return;
+                }
+                const feature = response.body.features[0];
+                const popupText = ``;
+                const popup = new mapboxgl.Popup({ closeOnClick: false, closeOnMove: false })
+                .setLngLat(feature.center)
+                .setHTML('<h1>This Location Details</h1>')
+                .addTo(map.current);
+                new mapboxgl.Marker().setLngLat(feature.center).setPopup(popup).addTo(map.current);
+              })
+          ))
       )
       .catch(console.log);
     })
-
 
   return (
     <div ref={mapContainer} className="map-container" />
